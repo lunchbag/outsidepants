e3fd477422e34b823a8ec1934118c1fc350cdae4
Andy Jiang <andyjiang@gmail.com>
2013-08-04 01:43:40 -0700

    added sending sms notifications if submit lost request

--- app/models/lost_item.rb
+++ app/models/lost_item.rb
@@ -1,16 +1,21 @@
 class LostItem
   include Mongoid::Document
 
-  field :product, type: String
   field :phone_number, type: String
   field :keywords, type: Array
+  field :product, type: String
 
-  def self.find_matches(keywords)
+  def self.find_numbers_by_product_and_keywords(product, keywords=[])
   	# Receives an array of keywords.
   	# Returns an array of phone numbers of matches.
-  	keywords.each do |keyword|
-  		# For each keyword, see if any exist in LostItem.
-  		
-  	end
+    array_of_phone_numbers = []
+    self.where(product: product).each do |found_item|
+      
+      # unless (keywords & found_item.keywords).empty?
+        array_of_phone_numbers.push(found_item.phone_number)
+        puts found_item.phone_number
+      # end
+    end
+    return array_of_phone_numbers
   end
 end
