111c73d3cb6eb1477f463174a63d4e2ea7823dbf
Andy Jiang <andyjiang@gmail.com>
2013-08-03 18:45:14 -0700

    added twilio config file

---
+++ config/initializers/twilio_config.rb
@@ -0,0 +1,3 @@
+Outsidehacks::Application.config.account_sid = "ACad465e648f6f424ea9293be34818ebf9"
+Outsidehacks::Application.config.auth_token = "be9ed87202b81e218b26e5423f7b6367"
+Outsidehacks::Application.config.phone_number = "+14155247714"
