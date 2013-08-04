a9d9335ea0efa70627628201716fcfd6ba39abec
Andy Jiang <andyjiang@gmail.com>
2013-08-03 23:09:13 -0700

    adding methods to models for matching

--- app/models/lost_item.rb
+++ app/models/lost_item.rb
@@ -5,4 +5,12 @@ class LostItem
   field :phone_number, type: String
   field :keywords, type: Array
 
+  def self.find_matches(keywords)
+  	# Receives an array of keywords.
+  	# Returns an array of phone numbers of matches.
+  	keywords.each do |keyword|
+  		# For each keyword, see if any exist in LostItem.
+  		
+  	end
+  end
 end
